35882ad06b4a8ef2b23bbeeed10d850260a96b10
vatsalsingh25 <133035358+vatsalsingh25@users.noreply.github.com>
2024-07-26 13:56:16 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Min Cost Climbing Stairs/min-cost-climbing-stairs.cpp
+++ Difficulty: Easy/Min Cost Climbing Stairs/min-cost-climbing-stairs.cpp
@@ -11,22 +11,28 @@ using namespace std;
 class Solution {
   public:
     int func(vector<int>& cost, int ind, vector<int>&dp){
-        if(ind==0) return 0;
-        if(ind==1) return 0;
+        if(ind>=cost.size())
+        {
+            return 0;
+        }
         
         if(dp[ind]!=-1) return dp[ind];
         
-        int oneStep = func(cost,ind-1,dp)+cost[ind-1];
-        int twoStep = func(cost,ind-2,dp)+cost[ind-2];
+        int oneStep = func(cost,ind+1,dp)+cost[ind];
+        int twoStep = func(cost,ind+2,dp)+cost[ind];
         
         return dp[ind]=min(oneStep,twoStep);
     }
     int minCostClimbingStairs(vector<int>&cost ,int N) {
         //Write your code here
         vector<int>dp(N+1,-1);
-        return func(cost,N,dp);
+        int a1= func(cost,0,dp);
+        vector<int>dp1(N+1,-1);
+        int a2=func(cost,1,dp1);
+        return min(a1,a2);
     }
 };
+
 
 //{ Driver Code Starts.
 
