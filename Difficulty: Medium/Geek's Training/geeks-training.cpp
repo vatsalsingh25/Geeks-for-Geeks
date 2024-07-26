2ff5a6d8c16d0d3dbb4d32926ef2732e41bd7b34
vatsalsingh25 <133035358+vatsalsingh25@users.noreply.github.com>
2024-07-26 19:17:55 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Geek's Training/geeks-training.cpp
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int func(vector<vector<int>>& arr, int ind, int last, vector<vector<int>>& dp){
+        if(ind==0){
+            int maxi=0;
+            for(int i=0; i<=2; i++){
+                if(i!=last) maxi = max(maxi, arr[0][i]);
+            }
+            return maxi;
+        }
+        
+        if(dp[ind][last]!=-1) return dp[ind][last];
+        
+        int maxi=0;
+        for(int i=0; i<=2; i++){
+            if(i!=last){
+                int points = arr[ind][i]+func(arr,ind-1,i,dp);
+                maxi=max(maxi, points);
+            }
+        }
+        return dp[ind][last]=maxi;
+    }
+    int maximumPoints(vector<vector<int>>& arr, int n) {
+        // Code here
+        vector<vector<int>>dp(n,vector<int>(4,-1));
+        return func(arr,n-1, 3, dp);
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<vector<int>> arr;
+        for (int i = 0; i < n; ++i) {
+            vector<int> temp;
+            for (int j = 0; j < 3; ++j) {
+                int x;
+                cin >> x;
+                temp.push_back(x);
+            }
+            arr.push_back(temp);
+        }
+
+        Solution obj;
+        cout << obj.maximumPoints(arr, n) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
