aacbd4d2fcc413368d68c0ef38b2e1daeeb71a15
vatsalsingh25 <133035358+vatsalsingh25@users.noreply.github.com>
2024-07-29 12:37:49 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Knapsack with Duplicate Items/knapsack-with-duplicate-items.cpp
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    int func(int ind, int W, int val[], int wt[], vector<vector<int>>&dp){
+        if(ind==0){
+            return val[0]*(W/wt[0]); //val * no.of times it can be picked
+            // if(W>=wt[ind]) return val[ind];          //not thid because an item can be taken many times not ony once
+            // else return 0;                     
+        }
+        
+        if(dp[ind][W]!=-1) return dp[ind][W];
+        
+        int notPick = 0 + func(ind-1,W,val,wt,dp);
+        int pick = INT_MIN;
+        if(W>=wt[ind]) pick =val[ind] + func(ind,W-wt[ind],val,wt,dp);
+        
+        return dp[ind][W]=max(pick,notPick);
+    }
+    int knapSack(int N, int W, int val[], int wt[])
+    {
+        // code here
+        vector<vector<int>>dp(N,vector<int>(W+1,-1));
+        return func(N-1,W,val,wt,dp);
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int N, W;
+        cin>>N>>W;
+        int val[N], wt[N];
+        for(int i = 0;i < N;i++)
+            cin>>val[i];
+        for(int i = 0;i < N;i++)
+            cin>>wt[i];
+        
+        Solution ob;
+        cout<<ob.knapSack(N, W, val, wt)<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
