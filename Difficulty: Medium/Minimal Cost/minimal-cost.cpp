c8844c34475284f368ea07287dc061b9199f34af
vatsalsingh25 <133035358+vatsalsingh25@users.noreply.github.com>
2024-08-31 19:36:56 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimal Cost/minimal-cost.cpp
+++ Difficulty: Medium/Minimal Cost/minimal-cost.cpp
@@ -6,44 +6,54 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
-    int f(vector<int>&height, int k, int ind, vector<int>&dp){
+  
+    int func(vector<int>& arr, int& k, int ind, vector<int>&dp){
         if(ind==0) return 0;
         
-        if(dp[ind]!=0) return dp[ind];
+        if(dp[ind]!=-1) return dp[ind];
         
         int mini = INT_MAX;
         for(int i=1; i<=k; i++){
             if(ind>=i){
-                int jumpCost = f(height,k,ind-i,dp)+abs(height[ind]-height[ind-i]);
+                int jumpCost=func(arr,k,ind-i,dp)+abs(arr[ind]-arr[ind-i]);
                 mini = min(mini,jumpCost);
-            }
+            } 
         }
         return dp[ind]=mini;
     }
-    int minimizeCost(vector<int>& height, int n, int k) {
+    int minimizeCost(vector<int>& arr, int& k) {
         // Code here
-        vector<int>dp(n,0);
-        f(height,k,n-1,dp);
-        return f(height,k,n-1,dp);
+        int n = arr.size();
+        vector<int>dp(n,-1);
+        return func(arr,k,n-1,dp);
     }
 };
-
 
 //{ Driver Code Starts.
 
 int main() {
-    int t;
-    cin >> t;
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
     while (t--) {
-        int N, K;
-        cin >> N >> K;
-        vector<int> arr(N);
-        for (int i = 0; i < N; i++) {
-            cin >> arr[i];
+        string ks;
+        getline(cin, ks);
+        int k = stoi(ks);
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
         }
         Solution obj;
-        cout << obj.minimizeCost(arr, N, K) << "\n";
+        int res = obj.minimizeCost(arr, k);
+        cout << res << endl;
+        // string tl;
+        // getline(cin, tl);
     }
     return 0;
 }
+
 // } Driver Code Ends
