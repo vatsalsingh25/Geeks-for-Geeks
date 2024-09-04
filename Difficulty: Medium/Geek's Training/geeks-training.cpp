383487f178103ef6229f20508786b2bd7e03298f
vatsalsingh25 <133035358+vatsalsingh25@users.noreply.github.com>
2024-09-04 13:36:25 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Geek's Training/geeks-training.cpp
+++ Difficulty: Medium/Geek's Training/geeks-training.cpp
@@ -6,30 +6,32 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
-    int func(vector<vector<int>>& arr, int ind, int last, vector<vector<int>>& dp){
+    int func(vector<vector<int>>& arr, int last, int ind, vector<vector<int>>&dp){
         if(ind==0){
-            int maxi=0;
-            for(int i=0; i<=2; i++){
-                if(i!=last) maxi = max(maxi, arr[0][i]);
+            int maxi = 0;
+            for(int i=0; i<3; i++){
+                if(i!=last) maxi = max(maxi,arr[0][i]);
             }
             return maxi;
-        }
+        } 
         
         if(dp[ind][last]!=-1) return dp[ind][last];
         
         int maxi=0;
-        for(int i=0; i<=2; i++){
+        for(int i=0; i<3; i++){
             if(i!=last){
-                int points = arr[ind][i]+func(arr,ind-1,i,dp);
-                maxi=max(maxi, points);
-            }
+                int points = arr[ind][i]+func(arr,i,ind-1,dp);
+                maxi = max(maxi,points);                
+            } 
         }
         return dp[ind][last]=maxi;
     }
+    
     int maximumPoints(vector<vector<int>>& arr, int n) {
         // Code here
+        // int last = -1;
         vector<vector<int>>dp(n,vector<int>(4,-1));
-        return func(arr,n-1, 3, dp);
+        return func(arr,3,n-1,dp);
     }
 };
 
