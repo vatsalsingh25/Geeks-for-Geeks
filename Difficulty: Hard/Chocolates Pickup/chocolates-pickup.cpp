014281de55004846f2a1ca509efe4a6d7f22169a
vatsalsingh25 <133035358+vatsalsingh25@users.noreply.github.com>
2024-09-05 11:52:45 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Chocolates Pickup/chocolates-pickup.cpp
+++ Difficulty: Hard/Chocolates Pickup/chocolates-pickup.cpp
@@ -6,34 +6,37 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
-    int func(int i, int j1, int j2, vector<vector<int>>& grid, vector<vector<vector<int>>>&dp){
+    int func(vector<vector<int>>& grid, int i, int j1, int j2, vector<vector<vector<int>>>&dp){
         int n = grid.size();
         int m = grid[0].size();
-        if(j1<0 || j1>=m || j2<0 || j2>=m) return -1e9;
+        
+        if(j1<0 || j2<0 || j1>=m || j2>=m) return -1e9;
         if(i==n-1){
-            if(j1==j2) return grid[i][j1];
-            else return grid[i][j1]+grid[i][j2];
+            if(j1==j2) return grid[n-1][j1];
+            else{
+                return grid[n-1][j1]+grid[n-1][j2];
+            }
         }
         
         if(dp[i][j1][j2]!=-1) return dp[i][j1][j2];
         
         int maxi = -1e9;
-        for(int dj1=-1; dj1<=1; dj1++){
-            for(int dj2=-1; dj2<=1; dj2++){
+        for(int d1=-1; d1<=1; d1++){                        //direction of move in the next row for 1st robot
+            for(int d2=-1; d2<=1; d2++){                    //direction of move in the next row for 2st robot
                 int value;
-                if(j1==j2) value=grid[i][j1];
-                else value = grid[i][j1]+grid[i][j2];
-                value+=func(i+1,j1+dj1,j2+dj2,grid,dp);
-                maxi=max(maxi,value);
+                if(j1==j2) value = grid[i][j1]+func(grid,i+1,j1+d1,j2+d2,dp);
+                else value = grid[i][j1]+grid[i][j2]+func(grid,i+1,j1+d1,j2+d2,dp);
+                
+                maxi = max(maxi,value);
             }
         }
         return dp[i][j1][j2]=maxi;
     }
+    
     int solve(int n, int m, vector<vector<int>>& grid) {
         // code here
-        //dp[n][m][m]
         vector<vector<vector<int>>>dp(n,vector<vector<int>>(m,vector<int>(m,-1)));
-        return func(0,0,m-1,grid,dp);
+        return func(grid, 0, 0, m-1,dp);
     }
 };
 
